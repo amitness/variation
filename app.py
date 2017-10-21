e78061156b85050b19fb2eab358ff7dd7926f914
Amit <meamitkc@gmail.com>
2017-10-21 22:55:42 +0545

    Initialize template structure

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return "Homepage"
+    return render_template('index.html')
+
 
 def main():
-    app.run('0.0.0.0', 8000)
+    app.run('0.0.0.0', debug=True)
 
 if __name__ == '__main__':
     main()
